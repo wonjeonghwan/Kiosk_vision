32b0af64ae5ac57550c7034ebf8802244d0d62b2
WONJEONGHWAN <wonjeonghwan@naver.com>
2025-03-14 22:34:26 +0900

    function noted

--- models/db_manager.py
+++ models/db_manager.py
@@ -36,23 +36,23 @@ def save_face(name, encodings):
 
 def find_best_match(encoding, threshold=0.45):
     """ 기존 사용자와 얼굴 매칭 """
-    conn = sqlite3.connect(DB_PATH)
-    cursor = conn.cursor()
-    cursor.execute("SELECT id, name, encoding FROM faces")
-    rows = cursor.fetchall()
-    conn.close()
+    conn = sqlite3.connect(DB_PATH)     # DB 연결
+    cursor = conn.cursor()              # 커서객체 생성(SQL 실행)
+    cursor.execute("SELECT id, name, encoding FROM faces")  # SQL문 실행
+    rows = cursor.fetchall()            # 가져온 모든 값을 rows에 저장
+    conn.close()                        # DB 연결 닫기
 
-    best_match_id = None
-    best_match_name = None
-    min_distance = threshold
+    best_match_id = None        #빈 변수
+    best_match_name = None      #빈 변수
+    min_distance = threshold    #입력한 값(0.45)
 
-    for row in rows:
-        stored_encoding = pickle.loads(row[2])  # 바이너리 데이터를 Numpy 배열로 변환
-        distance = np.linalg.norm(encoding - stored_encoding)
+    for row in rows:    # 모든 DB정보에 대한 for문
+        stored_encoding = pickle.loads(row[2])  # 벡터화 한 데이터를 다시 numpy로 변환한 것을 변수에 넣고
+        distance = np.linalg.norm(encoding - stored_encoding)   # 카메라로 받은 numpy값과 저장된 numpy의 차이를 변수에 넣고
 
-        if distance < min_distance:
-            min_distance = distance
-            best_match_id = row[0]
-            best_match_name = row[1]
+        if distance < min_distance:     # 만약 해당 row가 입력값보다 차이가 적다면
+            min_distance = distance     # 낮은 차이를 최소 값으로 지정 (가장 차이가 적은(맞는) 얼굴을 찾아감)
+            best_match_id = row[0]      # id
+            best_match_name = row[1]    # 이름(입력값)
 
     return best_match_id, best_match_name
